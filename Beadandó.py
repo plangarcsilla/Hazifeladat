36794db6484d09d6a80e12be0153aaa45efda0ca
plangarcsilla <36527996+plangarcsilla@users.noreply.github.com>
2018-02-27 20:24:16 +0100

    19-es feladattól a 25-ös feladatig.

--- Beadandó.py
+++ Beadandó.py
@@ -220,3 +220,95 @@ def main():
     print(feladat17(333))
 if __name__ == '__main__':
     main()
+
+
+def feladat19(n):
+    n=int(input("Szám: "))
+    prim=True
+    if n==1:
+        prim=False
+    for i in range(2,n):
+        if n%i==0:
+            prim=False
+            break
+        if prim:
+            print("Prímszám")
+        else:
+            print("Nem prímszám")
+
+def main():
+    feladat19(10)
+if __name__ == '__main__':
+    main()
+
+
+def feladat20(n):
+    a=1
+    b=1
+    if n==1:
+        print(a, end=" ")
+    elif n==2:
+        print(a,b, end=" ")
+    else:
+        c=a+b
+        print(a,b,c, end=" ")
+        k=3
+        while k<n:
+            a=b
+            b=c
+            c=a+b
+            print(c, end=" ")
+            k+=1
+
+def main():
+    feladat20(10)
+if __name__ == '__main__':
+    main()
+
+
+def feladat21(n):
+    ujszam=0
+    while n!=0:
+        maradek=n%10
+        ujszam=ujszam*10+maradek
+        n=n/10
+    return ujszam
+
+def main():
+    print(feladat21(7532))
+if __name__ == '__main__':
+    main()
+
+
+def feladat22(a,b,c):
+    c=1
+    while b>0:
+        if b%2==1:
+            c=c*a
+            b=b-1
+        else:
+            a=a*a
+            b=[b/2]
+    return c
+
+def main():
+    feladat22(2,3,4)
+if __name__ == '__main__':
+    main()
+
+
+def feladat25():
+    a=input("Add meg az ország lakosainak számát ezer főben: ")
+    b=input("Add meg az ország területét négyzetkilométerben:")
+    c=a%b*1000
+    if c<50:
+        print("Ritkán lakott")
+    elif c>=50 and c<300:
+        print("Álagos népsűrűségű")
+    else:
+        print("Sűrűn lakott")
+
+def main():
+    feladat25()
+if __name__ == '__main__':
+    main()
